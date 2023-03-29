91a8ca201b50ac5014c18e49816cb08c75cab17d
haecheol-shin <dev.hc.shin@gmail.com>
2023-03-29 22:56:14 +0900

    2023.03.29

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,21 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice); // 주문은 할인에 대해 모른다. 단일 책임 원칙이 잘 지켜진 것이다.
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
