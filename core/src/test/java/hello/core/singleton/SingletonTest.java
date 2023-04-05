f162d7b3d945b30ed76a82a3083ac079c2e43b44
haecheol-shin <dev.hc.shin@gmail.com>
2023-04-05 23:07:40 +0900

    2023.04.05

---
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,33 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+        //1. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        //2. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        // 참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+
+        // 이렇게 하면 memberService가 memberRepository를 의존하고 있기때문에
+        // 총 4개의 객체가 생성된다. -> 너무 많은 객체의 생성은 메모리 낭비를 만든다.
+        // 그래서 싱글톤 패턴을 적용해야 한다. ( 한개의 인스턴스를 생성해서 공유한다.)
+    }
+}
