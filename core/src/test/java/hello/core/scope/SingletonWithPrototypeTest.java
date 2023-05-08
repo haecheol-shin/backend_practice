8e03fc9bfa2368d74056701430bb70299e4680b8
haecheol-shin <dev.hc.shin@gmail.com>
2023-05-08 23:49:13 +0900

    2023.05.08

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
@@ -2,12 +2,14 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -37,20 +39,22 @@ public class SingletonWithPrototypeTest {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
-
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        // private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        // 스프링에서 제공하는 Provider(스프링에 의존적이다.)
+
+        private Provider<PrototypeBean> prototypeBeanProvider;
+        // 자바에서 표준으로 지원함. 라이브러리가 필요하다.
+        // 스프링이 아닌 다른 컨테이너에서도 사용 가능하다.
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
