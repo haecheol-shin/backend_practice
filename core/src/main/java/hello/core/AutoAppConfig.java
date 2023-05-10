c581b647cccdac735919f5aae2988cc6e0312001
haecheol-shin <dev.hc.shin@gmail.com>
2023-05-10 23:37:29 +0900

    2023.05.10

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -3,7 +3,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import static org.springframework.context.annotation.ComponentScan.*;
-@Configuration@ComponentScan(
+@Configuration
+@ComponentScan(
+        basePackages = "hello.core",
+        // basePackages를 설정하지 않으면
+        // @ComponantScan이 붙은 설정 정보 클래스의 패키지가 시작위치
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes =
                 Configuration.class))
 public class AutoAppConfig {
