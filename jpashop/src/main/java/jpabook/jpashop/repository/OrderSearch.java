36275d8bbe5f6bcfbc1129c198d233c3e890fba4
haecheol-shin <dev.hc.shin@gmail.com>
2023-05-18 23:44:32 +0900

    2023.05.18

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderSearch.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -11,7 +11,4 @@ public class OrderSearch {
     private String memberName; // 회원 이름
     private OrderStatus orderStatus; // 주문상태 (ORDER, CANCEL)
 
-    public Order findOne(Long id) {
-        return em.f
-    }
 }
